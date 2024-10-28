1181658344c575fb5e4979db770b14b414f7dc8f
EduSalazar0 <edutebansalazar95@gmail.com>
2024-10-28 07:51:43 -0500

    Creación de Clases con sus respectivos atributos y etiquetas.

--- SalazarEduardo_ExamenProgreso1/Models/Celular.cs
+++ SalazarEduardo_ExamenProgreso1/Models/Celular.cs
@@ -1,6 +1,19 @@
-﻿namespace SalazarEduardo_ExamenProgreso1.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SalazarEduardo_ExamenProgreso1.Models
 {
     public class Celular
     {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [MinLength(1)]
+        [MaxLength(100)]
+        public string Modelo { get; set; }
+        public int Anio { get; set; }
+        [Range(100.00, 999.00,
+            ErrorMessage = "El precio debe ir 100.00 and 999.00")]
+        public double Precio { get; set; }
+
     }
 }
--- SalazarEduardo_ExamenProgreso1/Models/ESalazar.cs
+++ SalazarEduardo_ExamenProgreso1/Models/ESalazar.cs
@@ -1,8 +1,25 @@
-﻿namespace SalazarEduardo_ExamenProgreso1.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data;
+using System.Diagnostics.CodeAnalysis;
+
+namespace SalazarEduardo_ExamenProgreso1.Models
 {
     public class ESalazar
     {
-        
-        
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(100)]
+        public string Nombre { get; set; } = null!;
+        [NotNull]
+        public double Sueldo { get; set; }
+        public bool TieneNovia { get; set; }
+        public DateTime Fecha { get; set; }
+        [ForeignKey("Celular")]
+        public int IdCelular {  get; set; }
+        public virtual Celular? Celular { get; set; } = null!;
+
+
     }
 }
